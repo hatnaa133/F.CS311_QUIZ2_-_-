0896d7190c6edd7d1c348fe100ab1f5743b422e5
Lab216-26 <Lab216-26@Lab216-28-PC>
2023-05-05 13:57:55 +0800

    hatnaa

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Course course = new Course("Software Construction");
+		course.setCourseName("Software Construction");
+		System.out.println(course.courseName);
+		System.out.println(course.getCourseName());
+		course.setCourseName("Программ хангамжийн бүтээл");
+		System.out.println(course.getCourseName());
+	}
+
+}
